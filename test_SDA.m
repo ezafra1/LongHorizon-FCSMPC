6e84ef8b4d6840e9ed607188d470d8800f542862
ezafra1 <70198357+ezafra1@users.noreply.github.com>
2023-04-27 10:57:03 +0200

    Add files via upload

--- test_SDA.m
+++ test_SDA.m
@@ -2,15 +2,15 @@ clear all
 clc
 %% Original FCS
 % \V = \{ u_{\min},...,u_{\max} \} \subset \Z
-u_min=0;
-u_max=1;
+u_min=0;    %Minimum value for input
+u_max=1;    %Maximum value for input
 
 %% Multistep FCS
 % u \in \V^n_u \subset \Z^n_u
 % \U_k = \V^n_u Np \subset \Z^p; p=n_u Np
 
 n_u = 3;             % Number of inputs u=[Sa Sb Sc]'
-Np = 5;              % Prediction Horizon length
+Np = 7;              % Prediction Horizon length
 p = n_u*Np;          % Length of switching sequences U_k
 Kbest = 8;           % Kb parameter for K-best SDA
 Niter_max = 32000;   % Maximum number of explored nodes for SDA
@@ -28,13 +28,17 @@ Tr = sqrt(2/3)*[1     -0.5         -0.5;      % Clarke transform
 
 %% Continuous-Time alpha-beta Model -> 2 lvl VSI with output LC filter as example
 
-n_y = 2;               % Number of outputs for control y= [vo_alpha vo_beta]
+n_y = 2;             % Number of outputs for control y= [vo_alpha vo_beta]
 Ts = 50e-6;          % Sample Time
 Lf = 0.002;          % LC filter inductor
 Cf = 50e-6;          % LC filter capacitor
 w = 2*pi*50;         % Angular frequency (@50 Hz)
 Vdc = 400;           % Dc link voltage
+Vo_ref_amp = 110;    % Output voltage amplitude reference
 lambda = 60.0;       % Weighting factor: Switching effort penalty
+
+Rr=[1  -w*Ts;        % Rotation matrix
+     w*Ts  1];
 
 Ac_xy = [O_2        -(1/Lf)*I_2         O_2     ;
       (1/Cf)*I_2      O_2       -(1/Cf)*I_2   ;
@@ -64,17 +68,45 @@ C(1:n_y,3:4) = eye(2);
 Uop_ESA = zeros(p,1); 
 Uop_SDA = zeros(p,1);
 Uop_Kbest = zeros(p,1);
-Niter=0.0;          % Count nodes
+F_k=zeros(p,1);
+Niter=0.0;           % Count nodes
 Niter_ac=0.0;
 Niter_avg=0.0;
-sim = 0;            % Number of simulations
+sim = 0;             % Number of simulations
 simMAX = 1;
+uabc_k_1 = [0 0 0]'; % Assume previous state is 0 0 0
 
 while (sim < simMAX)
 
-%% Unconstrained solution (randomly generated in this example)
-U_unc = 4*(rand(p,1)-0.5);
-U_unc = min(u_max+1, max(u_min-1, U_unc));
+%% Vo Reference
+theta = 2*pi*(rand(1));     % Syncronization angle, generated randomly in this example
+Vo_abc_ref = Vo_ref_amp*[sin(theta); sin(theta-2*pi/3); sin(theta+2*pi/3)];
+Vo_xy_ref = Tr*Vo_abc_ref;  % Vo reference in alpha-beta
+
+%% Output sequences
+Yref=zeros(n_y*Np,1);
+Yref(1:2,1)=Rr*Vo_xy_ref;   % Construct output reference sequence
+
+for k=n_y+1:n_y:n_y*Np
+    row_i=k;
+    row_f=k+n_y-1;
+   
+    Yref(row_i:row_f,:)=Rr*Yref(row_i-n_y:row_f-n_y,:);
+end
+
+%% System state
+Vo_xy = Vo_xy_ref + 20*(rand(1)-0.5);       % Assume Vo around reference with +-10 V error
+Io_xy = [0 0]';                             % Assume output current is zero
+Ii_xy = Io_xy + (Cf/Ts)*(Rr*Vo_xy-Vo_xy);   % Approximate filter current
+
+x_k = [Ii_xy ; Vo_xy; Io_xy];               % System state variables
+
+
+%% Unconstrained solution calculation
+F_k=UpsilonT*(Gamma*x_k-Yref)-lSTE*uabc_k_1;
+U_unc=-W_inv*F_k;
+%U_unc = 4*(rand(p,1)-0.5); % To generate randomly
+%U_unc = min(u_max+1, max(u_min-1, U_unc)); % Limit unconstrained values
 U_bar_unc = H*U_unc;          % Center of the sphere in the transformed space
 
 %% Initial Vector U_ini -> Only for SDA
